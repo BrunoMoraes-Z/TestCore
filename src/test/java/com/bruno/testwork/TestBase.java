c57ef8561b4dd1a43f379ebc1212bfedabef1031
Bruno Moraes <moraes.7bruno@gmail.com>
2020-12-13 16:12:40 -0300

    adicionado compatibilidade com testes Dekstop com Winium

--- src/test/java/com/bruno/testwork/TestBase.java
+++ src/test/java/com/bruno/testwork/TestBase.java
@@ -2,22 +2,58 @@ package com.bruno.testwork;
 
 import com.bruno.testwork.framework.TestType;
 import com.bruno.testwork.framework.Utils;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.winium.DesktopOptions;
+import org.openqa.selenium.winium.KeyboardSimulatorType;
+import org.openqa.selenium.winium.WiniumDriver;
 import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public abstract class TestBase {
 
-    private static TestType type = TestType.WEB_EDGE;
+    private TestType type;
+    private WebDriver driver;
+    private HashMap<TestType, WebDriver> drivers;
+    private String path, deployPath, process, applicationPath;
+    private Actions actions;
 
     public TestBase() {
-        String path = Utils.getProperty("pathDriver");
-        WebDriver driver = null;
+        this.drivers = new HashMap<>();
+        this.deployPath = System.getProperty("user.dir") + "\\deploys\\";
+        this.path = Utils.getProperty("pathDriver");
+    }
 
+    public void setDriverType(TestType type) {
+        this.setDriverType(type, false);
+    }
+
+    public TestBase setApplicationPath(String process, String applicationPath) {
+        this.process = process;
+        this.applicationPath = applicationPath;
+        return this;
+    }
+
+    public void setDriverType(TestType type, boolean defaultSite) {
+        if (this.type != null && this.driver != null) {
+            this.drivers.put(this.type, this.driver);
+        }
+        if (this.drivers.containsKey(type)) {
+            this.type = type;
+            this.driver = this.drivers.get(type);
+            return;
+        }
+        this.type = type;
         switch (type) {
             case WEB_CHROME:
                 System.setProperty(type.getDriverName(), path.concat("chromedriver.exe"));
@@ -37,34 +73,97 @@ public abstract class TestBase {
                 e_op.setCapability("chromium", true);
                 driver = new EdgeDriver(e_op);
                 break;
+            case DEKSTOP_WINIUM:
+                if (this.applicationPath == null) {
+                    System.out.println("Nenhum Aplicativo Informado. Utilize setApplicationPath(<path>)");
+                    break;
+                }
+                System.setProperty("webdriver.winium.driver.desktop", path.concat("winiumdriver.exe"));
+                DesktopOptions d_op = new DesktopOptions();
+                d_op.setApplicationPath(this.applicationPath);
+                d_op.setKeyboardSimulator(KeyboardSimulatorType.BasedOnWindowsFormsSendKeysClass);
+                d_op.toCapabilities();
+                startApplication(path.concat("winiumdriver.exe"));
+                try {
+                    driver = new WiniumDriver(new URL("http://localhost:9999"), d_op);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
         }
 
         if (TestType.isWeb(type) && driver != null) {
             if (Boolean.parseBoolean(Utils.getProperty("maximize"))) {
                 driver.manage().window().maximize();
             }
-            driver.get(Utils.getProperty("URL"));
+            if (defaultSite) {
+                driver.get(Utils.getProperty("URL"));
+            }
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            this.actions = new Actions(this.driver);
+        }
+    }
+
+    public WebDriver getDriver() {
+        return this.driver;
+    }
+
+    public void sleep(int milis) {
+        try {
+            Thread.sleep(milis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @BeforeSuite
+    protected void setup() {
+        System.out.println("Iniciando...");
+    }
+
+    @AfterSuite
+    protected void teardown() {
+        System.out.println("Finalizando...");
+        sleep(1000);
+        killTask("chromedriver.exe");
+        killTask("geckdriver.exe");
+        killTask("msedgedriver.exe");
+        killTask("operadriver.exe");
+        killTask("winiumdriver.exe");
+        if (this.process != null) {
+            killTask(process);
+        }
+    }
+
+    public void killTask(String executable) {
+        try {
+            Runtime.getRuntime().exec("taskkill /f /im " + executable);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void startApplication(String appPath) {
+        try {
+            Runtime.getRuntime().exec("cmd /c " + appPath);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 
     @BeforeSuite
     public void suiteSetup() {
-        System.out.println("suite setup");
     }
 
     @BeforeMethod
     public void testSetup() {
-        System.out.println("test setup");
     }
 
     @AfterSuite
     public void suiteTeardown() {
-        System.out.println("suite teardown");
     }
 
     @AfterMethod
     public void testTeardown() {
-        System.out.println("test teardown");
     }
 
 
--- src/test/java/com/bruno/testwork/suites/teste.java
+++ src/test/java/com/bruno/testwork/suites/teste.java
@@ -1,18 +1,33 @@
 package com.bruno.testwork.suites;
 
 import com.bruno.testwork.TestBase;
+import com.bruno.testwork.framework.TestType;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class teste extends TestBase {
 
     @Test
     public void teste01() {
-        System.out.println("teste");
+        setApplicationPath("Calculator.exe", "C:/Windows/System32/calc.exe").setDriverType(TestType.DEKSTOP_WINIUM);
+
+        By um = By.id("num1Button");
+        getDriver().findElement(um).click();
+        getDriver().findElement(um).click();
+        getDriver().findElement(um).click();
+        sleep(2000);
     }
 
-    @Test
-    public void teste02() {
-        System.out.println("teste");
-    }
+//    @Test
+//    public void teste02() {
+//        setDriverType(TestType.WEB_CHROME);
+//        getDriver().get("https://google.com");
+//    }
+//
+//    @Test
+//    public void teste03() {
+//        setDriverType(TestType.WEB_EDGE);
+//        getDriver().get("https://www.branitube.net/watch/30979/quanzhi-gaoshou-2");
+//    }
 
 }
